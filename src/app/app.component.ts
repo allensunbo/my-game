43078f7f58968a467b70cc3d171c98278e7df550
Bo Sun <bsun@axioma.com>
2018-10-30 21:45:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-game';
+  card = 'assets/PNG/blue_back.png';
+
+  ownCards = [];
+  opponentCards = [];
+
+  ngOnInit() {
+    this.ownCards = [];
+    for (let i = 2; i < 11; i++) {
+      this.ownCards.push({ rank: i, show: false, suit: 'C' });
+    }
+    this.opponentCards = [];
+    for (let i = 2; i < 11; i++) {
+      this.opponentCards.push({ rank: i, show: false, suit: 'D'  });
+    }
+  }
+
+  flip(card) {
+    card.show = true;
+  }
+
+  getCardUrl(card) {
+    return `assets/PNG/${card.rank}${card.suit}.png`;
+  }
 }
